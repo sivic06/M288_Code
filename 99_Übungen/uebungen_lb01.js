a978e6b92ffab70c249c61d238a3948f51ed2f34
Benjamin Bertalan <github@brtln.com>
2024-09-26 13:59:24 +0200

    übungen vorbereitung prüfung

---
+++ 99_Übungen/uebungen_lb01.js
@@ -0,0 +1,24 @@
+// Was gibt das folgende Programm aus?
+const x = 15 + 5;
+let y = 10 + 10;
+y = y + 7;
+console.log(x < y);
+
+// Geben Sie den String '68°F entspricht 20°C' auf der Konsole mit JavaScript aus.
+___ fahrneheit = ___ ;
+___ celsius = ____;
+console.log(``);
+
+console.log(`${fahrneheit} und ${celsius}`);
+
+// Geben Sie das vierte Element des Arrays fruechte auf der Konsole aus.
+let fruechte = ['Apfel', 'Banane', 'Mango', 'Ananas', 'Kiwi', 'Erdbeere'];
+____
+
+// Füllen Sie alle Lücken aus, damit auf der Konsole nur ‘Der Hund bellt.’ erscheint.
+___ tier = 'Hund';
+if (____ === ___) {
+    console.log('Der Hund bellt.');
+} else if (____ === ____) {
+    console.log('Die Katze miaut.');
+}
